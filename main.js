aa044b280a36f2e23a6a7ad40dcc9e0ec93ab639
emono <ababababa0222@gmail.com>
2019-06-03 01:07:28 +0900

    fix section9

--- main.js
+++ main.js
@@ -3,12 +3,7 @@ var app = new Vue(
   {
     el: '#app',
     data: {
-        count: 0
-      },
-    methods: {
-      increment: function () {
-        this.count += 1000
+        type: "A"
       }
     }
-  }
 )
